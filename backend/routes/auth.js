c1afc402b59758a81f0f7fb0d1bb48be00d34dd0
Amar <jeetamar496@gmail.com>
2023-06-19 17:07:53 +0530

    middleware added to get the data from body

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,13 +1,15 @@
 const express=require('express');
 const router=express.Router();
+const User=require("../models/User")
  
 
-router.get('/',(req,res)=>{
-    obj={
-        a:"this is my first API",
-        number:34
-    }
-    res.json(obj);
+router.post('/',(req,res)=>{
+    console.log(req.body)
+    const user=User(req.body);
+    user.save();
+    
+    res.send(req.body);
+    
 })
 
 module.exports=router;
