ac92b07951b6480cc48190be73e62bc1ab0a53a1
Amar <jeetamar496@gmail.com>
2023-06-19 14:13:19 +0530

    mongoose connected and express setup

---
+++ backend/db.js
@@ -0,0 +1,16 @@
+const mongoose=require('mongoose');
+// const mongoURI="mongodb://localhost:27017";
+
+const connectToMongo=()=>{
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/test');
+    console.log("connected to mongoose Successfully!")
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+
+}
+
+module.exports=connectToMongo
