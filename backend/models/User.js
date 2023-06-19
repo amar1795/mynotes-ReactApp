3dfb6c46c82378d69acbb1cb7341da10932cd990
Amar <jeetamar496@gmail.com>
2023-06-19 21:46:43 +0530

    data validation added using express validator

--- backend/models/User.js
+++ backend/models/User.js
@@ -21,4 +21,7 @@ const UserSchema = new Schema({
       }
 });
 
-module.exports=mongoose.model('User',UserSchema)
+const User=mongoose.model('User',UserSchema);
+User.createIndexes();
+
+module.exports=User;
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,14 +1,32 @@
 const express=require('express');
+const { body, validationResult } = require('express-validator');
+
 const router=express.Router();
 const User=require("../models/User")
  
 
-router.post('/',(req,res)=>{
-    console.log(req.body)
-    const user=User(req.body);
-    user.save();
+router.post('/',[
+    body('name',"enter a valid name").isLength({min:3}),
+    body('email',"enter a valid email").isEmail(),
+    body('password',"enter a valid password").isLength({min:5}),
     
-    res.send(req.body);
+],
+(req,res)=>{
+    const result = validationResult(req);
+  if (!result.isEmpty()) {
+    
+     return res.status(400).send({ errors: result.array() });
+  }
+
+ 
+  User.create({
+      name:req.body.name,
+      email:req.body.email,
+      password:req.body.password,    
+    }).then(user=>res.json(user))
+    .catch(err=>{console.log(err)
+    res.json({error:"please enter the a unique email",message:err.message})})
+    
     
 })
 
