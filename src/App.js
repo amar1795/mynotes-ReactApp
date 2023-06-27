7fe266a013892b7a0febc2466f22105ad6a8d11b
Amar <jeetamar496@gmail.com>
2023-06-27 15:14:25 +0530

    created context api

--- src/App.js
+++ src/App.js
@@ -8,11 +8,13 @@ import {
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
+import NoteState from './context/notes/NoteState';
 
 
 function App() {
   return (
     <>
+    <NoteState>
     <BrowserRouter>
       <Navbar/>
       <Routes>
@@ -22,7 +24,7 @@ function App() {
       
     </BrowserRouter>
         
-        
+    </NoteState>
     </>
   );
 }
--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { useContext } from 'react';
+import NoteContext from '../context/notes/NoteContext';
 
 const About = () => {
+    const a=useContext(NoteContext);
+    useEffect(()=>{
+        a.update();
+
+    },[])
   return (
     <div>
-      this is about page
+      this is {a.state.name} and his  profession is {a.state.profession}
     </div>
   )
 }
